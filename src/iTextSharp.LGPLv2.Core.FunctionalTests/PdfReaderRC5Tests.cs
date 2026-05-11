9d69e797b051c6fd669fbf603326351a502c788c
christian.andrade <christian.andrade@softplan.com.br>
2026-05-11 16:44:17 -0300

    test: adiciona testes para PDFs RC5 com múltiplas páginas

--- src/iTextSharp.LGPLv2.Core.FunctionalTests/PdfReaderRC5Tests.cs
+++ src/iTextSharp.LGPLv2.Core.FunctionalTests/PdfReaderRC5Tests.cs
@@ -21,6 +21,17 @@ public class PdfReaderRC5Tests
         Assert.IsTrue(reader.IsEncrypted());
         Assert.IsTrue(reader.NumberOfPages > 0);
     }
+    
+    [TestMethod]
+    public void Deve_Abrir_Pdf_Com_Criptografia_R5_With_Two_Pages()
+    {
+        var caminho = TestUtils.GetPdfsPath("pdf_RC5_encryption_2pages.pdf");
+
+        using var reader = new PdfReader(File.ReadAllBytes(caminho));
+
+        Assert.IsTrue(reader.IsEncrypted());
+        Assert.IsTrue(reader.NumberOfPages > 0);
+    }
 
     [TestMethod]
     public void Deve_Abrir_Pdf_R5_Com_Restricao_De_Owner()
@@ -41,6 +52,22 @@ public class PdfReaderRC5Tests
         PdfReader.AllowOpenWithFullPermissions = false;
 
         var caminho = TestUtils.GetPdfsPath("pdf_RC5_encryption_2.pdf");
+        using var reader = new PdfReader(File.ReadAllBytes(caminho));
+        using var output = new MemoryStream();
+
+        var ex = Assert.ThrowsException<BadPasswordException>(
+            () => _ = new PdfStamper(reader, output));
+
+        Assert.AreEqual("PdfReader not opened with owner password", ex.Message);
+    }
+    
+    
+    [TestMethod]
+    public void Deve_Lancar_Erro_Ao_Modificar_Pdf_R5_2Pages_Sem_AllowOpenWithFullPermissions()
+    {
+        PdfReader.AllowOpenWithFullPermissions = false;
+
+        var caminho = TestUtils.GetPdfsPath("pdf_RC5_encryption_owner_readonly_2pages.pdf");
         using var reader = new PdfReader(File.ReadAllBytes(caminho));
         using var output = new MemoryStream();
 
